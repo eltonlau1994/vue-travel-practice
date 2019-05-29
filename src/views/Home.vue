1547b2feeaf69005cc63be162f1f0f2526688145
Elton Lau <twilightlau94@gmail.com>
2019-05-29 14:13:50 +0800

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <p>Hello world</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
