607ea1a7dae0b42c304ede0009eebcf35001d97c
elton_lau94 <twilightlau94@gmail.com>
2019-06-02 16:40:03 +0800

    completed city search box

--- src/views/City.vue
+++ src/views/City.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="city">
         <CityHeader></CityHeader>
+        <CitySearch></CitySearch>
     </div>
 </template>
 
 <script>
 import CityHeader from '@/components/CityHeader.vue'
+import CitySearch from '@/components/CitySearch.vue'
 
 export default {
     name: 'City',
     components: {
-        CityHeader
+        CityHeader,
+        CitySearch
     }
 }
 </script>
