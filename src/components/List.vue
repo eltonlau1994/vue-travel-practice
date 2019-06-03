71963b2514bb5bd0721c14d3dc268e752e8c2d63
elton_lau94 <twilightlau94@gmail.com>
2019-06-03 23:49:29 +0800

    completed layout

---
+++ src/components/List.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">Current City</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button"> Hong Kong</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">Popular Cities</div>
+            </div>        
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-topbottom">
+                        America
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+    mounted() {
+        this.scroll = new Bscroll(this.$refs.wrapper)
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/mixins.styl';
+    .borden-topbottom
+        &:before
+            border-color #ccc
+        &:after
+            border-color #ccc
+    .list
+        overflow hidden
+        position: absolute
+        top 1.58rem
+        left 0
+        right 0
+        bottom 0
+        .title
+            line-height .54rem
+            background #eee
+            padding-left .2rem
+            color #666
+            font-size .26rem
+        .button-list
+            overflow hidden
+            padding: .1rem
+            .button-wrapper
+                width 33.33%
+                float left
+                .button
+                    text-align center
+                    margin .1rem
+                    border .02rem solid #ccc
+                    padding .1rem 0
+        .item-list
+            .item
+                line-height .76rem
+                color #666
+                padding-left .2rem
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -2,18 +2,21 @@
     <div class="city">
         <CityHeader></CityHeader>
         <CitySearch></CitySearch>
+        <List></List>
     </div>
 </template>
 
 <script>
 import CityHeader from '@/components/CityHeader.vue'
 import CitySearch from '@/components/CitySearch.vue'
+import List from '@/components/List.vue'
 
 export default {
     name: 'City',
     components: {
         CityHeader,
-        CitySearch
+        CitySearch,
+        List
     }
 }
 </script>
