57c810e7509f7a8d862f1fa34a641e507637005a
Elton Lau <twilightlau94@gmail.com>
2019-06-16 10:50:09 +0800

    completed details

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,18 +2,44 @@
     <div class="detail">
         <DetailBanner></DetailBanner>
         <DetailHeader></DetailHeader>
-        <div class="content"></div>
+        <div class="content">
+            <DetailList :list="list"></DetailList>
+        </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from '@/components/DetailBanner'
 import DetailHeader from '@/components/DetailHeader'
+import DetailList from '@/components/DetailList'
 
 export default {
     components: {
         DetailBanner,
-        DetailHeader
+        DetailHeader,
+        DetailList
+    },
+    data () {
+        return {
+            list: [{
+                title: 'Adult Ticket',
+                children: [{
+                    title: 'Adult Ticket for Opera',
+                    children: [{
+                        title: 'Adult Ticket for Opera - Las Miserables'
+                    }]
+                }]
+            },
+            {
+                title: 'Student Ticket'
+            },
+            {
+                title: 'Children Ticket'
+            }, 
+            {
+                title: 'Special Discount Ticket'
+            }]
+        }
     }
 }
 </script>
